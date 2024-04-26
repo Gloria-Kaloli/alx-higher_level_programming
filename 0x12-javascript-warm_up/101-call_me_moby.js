a917c7d896617699ad3f0e22af8848ded9e76da7
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-04-26 09:09:06 -0700

    CALL ME MOBY

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,14 @@
-3
-rts.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
+#!/usr/bin/node
+
+module.exports = {
+  /**
+   * callMeMoby - Invokes a function x times.
+   * @param {Number} x - The number of times to invoke the function.
+   * @param {Number} theFunction - The function to be invoked.
+   */
+  callMeMoby: function (x, theFunction) {
+    for (let i = 0; i < x; i++) {
+      theFunction();
+    }
   }
 };
