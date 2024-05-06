76fb439958884cc452c6d0ab55b5032453f87935
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-05-06 14:43:04 -0700

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+//script that updates the text color of the header
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('header').style.color = '#FF0000';
+  }, false);
+})();
