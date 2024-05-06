7fea901a714c70133c614c664a6a4f679b1476a6
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-05-06 14:33:28 -0700

    Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+//script that fetches and lists the title for all movies by using this URL
+$(function () {
+  $.get('https://swapi.co/api/films/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      let films = data.results;
+      for (let i in films) {
+        $('#list_movies').append('<li>' + films[i].title + '</li>');
+      }
+    }
+  });
+});
