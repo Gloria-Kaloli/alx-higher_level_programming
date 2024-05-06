654265cfb8ad95144b50550f9807819204c97f7b
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-05-06 14:29:48 -0700

    Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+//JavaScript script that fetches the character name from this URL
+$(function () {
+  $.get('https://swapi.co/api/people/5/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('#character').text(data.name);
+    }
+  });
+});
